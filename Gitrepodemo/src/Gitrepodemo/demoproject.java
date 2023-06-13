17357b314c75f1ebcbab5583f9728e6194ae3deb
psdeo13 <110086127+psdeo13@users.noreply.github.com>
2023-06-14 03:13:30 +1000

    Update demoproject.java

--- Gitrepodemo/src/Gitrepodemo/demoproject.java
+++ Gitrepodemo/src/Gitrepodemo/demoproject.java
@@ -5,7 +5,7 @@ public class demoproject {
 	
     public static void main(String []args)
     {
-        System.out.println("Hello, Sajal, Padmanabh Deo!");
+        System.out.println("Hello, Sajal, Padmanabh!");
     }
 
 }
